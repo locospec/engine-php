5d8fb3dde831ee7c9d654f71845e18dc673a61e2
Arihant Jain <arihant@betalectic.com>
2025-03-27 16:50:56 +0530

    Added throw error if jq expression resolver fails

--- src/Database/AliasTransformation.php
+++ src/Database/AliasTransformation.php
@@ -45,7 +45,7 @@ class AliasTransformation
                 if (strpos($expression->source, '->') !== false) {
                     $source = preg_replace(['/^/', '/->?/'], ['.', '.'], $expression->source);
                 } else {
-                    $source = '.'.$source;
+                    $source = '.' . $source;
                 }
 
                 $extracted = $this->executeJQExpression($record, $source);
@@ -83,7 +83,7 @@ class AliasTransformation
 
             return trim($process->getOutput()) ?: null;
         } catch (\Exception $e) {
-            return null;
+            throw $e;
         }
     }
 }
