3161d6bc18554ac2d4f5aead642fd82e0453fe9f
rajeshn95 <rajesh@betalectic.com>
2025-04-08 19:11:51 +0530

    validation function added

--- src/Registry/ValidatorInterface.php
+++ src/Registry/ValidatorInterface.php
@@ -7,8 +7,20 @@ interface ValidatorInterface
     /**
      * Validate input data against the provided JSON schema.
      *
-     * @param  array  $currentOperation
+     * @param  array  $options
      * @return mixed True if validation passes, or errors collection.
      */
-    public function validate(array $input, array $schema, string $currentOperation);
+    public function validate(array $input, array $schema, array $options);
+    
+    /**
+     * Validate input data against the provided custom rules.
+     *
+     * @param string $rule  The custom rule name (without the "custom:" prefix).
+     * @param mixed  $value The value to validate.
+     * @param array  $options
+     * @return mixed True if validation passes, false otherwise.
+     */
+    public function validateCustomRule(string $rule, $value, array $options);
+
+    
 }
