12caa7ee3b936cddeaae2ac2ac6ad6a77c0f902f
rjvim <23579455+rjvim@users.noreply.github.com>
2025-07-10 11:06:36 +0000

    Fix styling

--- src/Tasks/DTOs/ReadPayload.php
+++ src/Tasks/DTOs/ReadPayload.php
@@ -5,12 +5,19 @@ namespace LCSEngine\Tasks\DTOs;
 class ReadPayload
 {
     public string $type = 'select';
+
     public string $modelName;
+
     public ?string $deleteColumn = null;
+
     public ?array $pagination = null;
+
     public array $sorts = [];
+
     public ?array $filters = null;
+
     public array $scopes = [];
+
     public array $expand = [];
 
     public function __construct(string $modelName)
@@ -21,6 +28,7 @@ class ReadPayload
     public function toArray(): array
     {
         $data = get_object_vars($this);
+
         return array_filter($data, fn ($value) => $value !== null);
     }
 }
--- src/Tasks/PayloadBuilders/ReadPayloadBuilder.php
+++ src/Tasks/PayloadBuilders/ReadPayloadBuilder.php
@@ -9,6 +9,7 @@ use LCSEngine\Tasks\Traits\PayloadPreparationHelpers;
 class ReadPayloadBuilder
 {
     use PayloadPreparationHelpers;
+
     protected ContextInterface $context;
 
     public function __construct(ContextInterface $context)
@@ -32,7 +33,7 @@ class ReadPayloadBuilder
         $this->prepareSortsForDto($payload, $readPayload, $primaryKeyAttributeKey);
 
         // Transfer filters from the incoming payload to the prepared payload.
-        if (isset($payload['filters']) && !empty($payload['filters'])) {
+        if (isset($payload['filters']) && ! empty($payload['filters'])) {
             $readPayload->filters = $payload['filters'];
         }
 
@@ -40,7 +41,7 @@ class ReadPayloadBuilder
         $readPayload->scopes = $this->context->get('query')->getAllowedScopes()->toArray();
 
         // Handle relationship expansion, using payload's expand if present, otherwise default from query.
-        if (isset($payload['expand']) && !empty($payload['expand'])) {
+        if (isset($payload['expand']) && ! empty($payload['expand'])) {
             $readPayload->expand = $payload['expand'];
         } else {
             $readPayload->expand = $this->context->get('query')->getExpand()->toArray();
